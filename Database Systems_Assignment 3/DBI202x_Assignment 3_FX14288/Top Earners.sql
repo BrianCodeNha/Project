031384161c90103f31973fbd85dbf7db3658ce11
BrianCodeNha <97515716+BrianCodeNha@users.noreply.github.com>
2022-03-01 08:56:18 +0700

    Add files via upload

---
+++ Database Systems_Assignment 3/DBI202x_Assignment 3_FX14288/Top Earners.sql
@@ -0,0 +1,5 @@
+select max(months*salary),count(months*salary) from employee 
+where employee_id in
+( select employee_id from employee where months*salary in 
+                        (select max(months*salary) from employee)
+);
